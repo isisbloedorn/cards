4dbef336f194e7e24e42876b933a4dbafe6a365d
isisleonilda <isis.bloedorn@escola.pr.gov.br>
2025-07-15 14:53:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,75 @@
+criaCartao(
+    'gereiras do k-pop',
+    'Qual o nome das cantoras do grupo huntr/x?',
+    'Rumi,Mira,Zoye.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Qual é o nome do grupo saja boys?',
+    'Jinu,Romance,Beby,Saja ,Baby e Mystery .'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'O que os saja boys são e representam ?',
+    'Os Saja Boys, por sua vez, são vilões demoníacos disfarçados, que devem ser combatidos pelas garotas.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Qual é o tempo do filme?',
+    '1 hora e 35 minutos.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Quem é o personagem do grupo Hunter/x é demonio?',
+    'Rumi.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Quais são as musicas do grupo saja boys?',
+    'Meu guarana e serei seu astro.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Quais são as musicas do grupo Hunter/x?',
+    'Brilho, letal minha voz,não tem perdão.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Quais são os animais de estimação de Jino?',
+    'Um tigre azul e um passarinho de chapéu.'
+)
+criaCartao(
+    'gereiras do k-pop',
+    'Casal ou trio do filme?',
+    'jino e ruim, beby e saja, zouy e mystery,mira & baby & romance.'
+)
+criaCartao(
+    'Agricultura',
+    'Qual a importância da agricultura familiar no Brasil?',
+    'A agricultura familiar desempenha um papel crucial no abastecimento do mercado interno brasileiro,sendo responsável por uma parte significativa da produção de alimentos e matérias-primas. '
+)
+criaCartao(
+    'Agricultura',
+    'O que é agricultura regenerativa?',
+    'A agricultura regenerativa busca restaurar a saúde do solo e do ecossistema, utilizando práticas como a rotação de culturas, o plantio direto e o uso de matéria orgânica. Acredita-se que essa prática possa ajudar a reverter as mudanças climáticas, pois o solo é um importante fixador de carbono. '
+)
+criaCartao(
+    'Agricultura',
+    'O que é agricultura?',
+    'A agricultura é a prática de cultivar o solo para produzir alimentos e matérias-primas.'
+)
+criaCartao(
+    'Agricultura',
+    'Quais são os principais produtos da agricultura brasileira?',
+    'Soja, cana-de-açúcar, algodão, milho, trigo e arroz,café, cacau, laranja e banana.'
+)
+criaCartao(
+    'Agricultura',
+    'O que é Agricultura familiar?',
+    'Realizada por famílias em pequenas propriedades, muitas vezes com foco na subsistência.'
+)
+criaCartao(
+    'Agricultura',
+    'O que é Agricultura orgânica ?',
+    'Prática que prioriza o cultivo sem uso de agrotóxicos e fertilizantes sintéticos.'
+)
